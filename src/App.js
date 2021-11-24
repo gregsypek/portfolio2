62f4aa7f003269ea71948a4c8183cc8b5e40b9eb
greg <greg.sypek@gmail.com>
2021-11-24 17:41:45 +0100

    add scss styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import "./scss/main.css";
 
 function App() {
-	return <div className="App">hello</div>;
+	return (
+		// <div className="App">
+		<nav className="home__nav">
+			<ul className="home__nav-list">
+				<li className="home__nav--name">Grzegorz Sypek</li>
+				<li className="home__nav--title">Frontend Developer</li>
+				<li className="home__nav--about">
+					<a href="#projects" className="home__nav--link">
+						Projects
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
 }
 
 export default App;
